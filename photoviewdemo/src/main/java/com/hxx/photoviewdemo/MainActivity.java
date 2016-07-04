b825cd76923aaf0440ceeedb218f7ee004726668
XiangxiangHou <hou_xiangxiang@126.com>
2016-07-04 23:44:39 +0800

    PhotoView填充Drawable

--- photoviewdemo/src/main/java/com/hxx/photoviewdemo/MainActivity.java
+++ photoviewdemo/src/main/java/com/hxx/photoviewdemo/MainActivity.java
@@ -3,11 +3,16 @@ package com.hxx.photoviewdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import uk.co.senab.photoview.PhotoView;
+
 public class MainActivity extends AppCompatActivity {
+    private PhotoView mPhotoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPhotoView = (PhotoView) findViewById(R.id.photoview);
+        mPhotoView.setImageDrawable(getResources().getDrawable(R.mipmap.ic_launcher));
     }
 }
